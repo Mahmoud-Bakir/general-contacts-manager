ba315c7c047ba7a2cc13bd1d1f5c23b376dd4d5e
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-06 23:49:42 +0300

    FE- added form component to app

--- contact-manager-fe/src/App.js
+++ contact-manager-fe/src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from './Pages/HomePage/homepage';
+import FormPage from './Pages/FormPage/formpage';
+
 
 function App() {
   return (
     <Routes>
        <Route path="/" element={<HomePage/>} />
+       <Route path="/add" element={<FormPage/>} />
     </Routes> 
   );
 }
