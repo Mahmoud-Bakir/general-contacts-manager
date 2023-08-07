3467366630e8e2f4da4408ba724d5ef55cda02e5
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-07 03:12:23 +0300

    FE- fixed container css

--- contact-manager-fe/src/Components/Card/card.jsx
+++ contact-manager-fe/src/Components/Card/card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./card.css";
 
-const Card = ({ name, number, altitude,longitde }) => {
+const Card = ({ name, number, altitude,longitude }) => {
   return (
     <div className="flex-card">
-      <p>{name}</p>
-      <p>{number}</p>
-      <p>{altitude}</p>
-      <p>{longitde}</p>
+      <span className="title">Personal info</span>
+      <span>Name: {name}</span>
+      <span>Phone number: {number}</span>
+      <br />
+      <span className="title">Location</span>
+      <span>Altitude: {altitude}</span>
+      <span>Longitude: {longitude}</span>
     </div>
   );
 };
